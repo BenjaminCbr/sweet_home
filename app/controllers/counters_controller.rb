cdc0bbf8e1ac4dc36d9eeabae4ad81e3f160727a
Benjamin (Benitott) Combourieu <benjamin.combourieu@gamil.com>
2017-06-24 18:03:20 +0200

    Home Page: design baseline

--- app/controllers/counters_controller.rb
+++ app/controllers/counters_controller.rb
@@ -1,5 +1,4 @@
 class CountersController < ApplicationController
-
   def destroy
     RedisModels::Counter.reset
     CounterChannel.broadcast_to('counter', counter_value: RedisModels::Counter.get)
